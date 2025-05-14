a629eab78197babc46da450e908c93937b0aab15
StribogCZ <weissewolfe616@gmail.com>
2025-05-14 09:54:10 +0200

    pojebalo se to

--- opentofu-project/main.tf
+++ opentofu-project/main.tf
@@ -25,7 +25,7 @@ provider "proxmox" {
 
 }
 
-resource "proxmox_vm_qemu" "cloudinit-test" {
+resource "proxmox_vm_qemu" "test1" {
     name = "terraform-test-vm"
     desc = "A test for using terraform and cloudinit"
 
@@ -68,7 +68,79 @@ resource "proxmox_vm_qemu" "cloudinit-test" {
                     #storage_type    = "rbd"
                     #iothread        = true
                     discard         = true
-                    replicate       = true
+                }
+            }
+        }
+    }
+
+    # Setup the network interface and assign a vlan tag: 256
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+        #tag = 256
+    }
+
+    # Setup the ip address using cloud-init.
+    boot = "order=scsi0"
+    # Keep in mind to use the CIDR notation for the ip.
+    #ipconfig0 = "ip=192.168.10.20/24,gw=192.168.10.1"
+    ipconfig0 = "ip=dhcp"
+    #sshkeys = <<EOF
+    #ssh-rsa 9182739187293817293817293871== user@pc
+    #EOF
+    serial {
+      id   = 0
+      type = "socket"
+    }
+    
+    ciuser = "debian"
+    cipassword = "debian"
+
+}
+
+resource "proxmox_vm_qemu" "cloudinit-test1" {
+    name = "terraform-test-vm2"
+    desc = "A test for using terraform and cloudinit"
+
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "pve"
+
+    # The destination resource pool for the new VM
+#    pool = "pool0"
+
+    # The template name to clone this vm from
+    clone = "deb12-tpl"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 2
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 2048
+    scsihw = "virtio-scsi-pci"
+
+    # Setup the disk
+    disks {
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "Drives"
+                }
+            }
+        }
+        scsi {
+            scsi0 {
+                disk {
+                    size            = 32
+                    cache           = "writeback"
+                    storage         = "Drives"
+                    #storage_type    = "rbd"
+                    #iothread        = true
+                    discard         = true
                 }
             }
         }
